3e1c6d39af3068122b07edf4cb963de255d48ac5
hcsc4vz4y5-droid <hcsc4vz4y5@privaterelay.appleid.com>
2026-05-29 01:24:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.SESSION_NAME === undefined ? "asitha" : process.env.SESSION_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? "94789121558" : process.env.OWNER_NUMBER,
+BOT_NAME: process.env.BOT_NAME === undefined ? "sasiya-md" : process.env.BOT_NAME,
+OWNER_NAME: process.env.OWNER_NAME === undefined ? "Sasiya" : process.env.OWNER_NAME,
+WORK_TYPE: process.env.WORK_TYPE === undefined ? "public" : process.env.WORK_TYPE,
+TIME_ZONE: process.env.TIME_ZONE === undefined ? "Asia/Colombo" : process.env.TIME_ZONE,
 };
