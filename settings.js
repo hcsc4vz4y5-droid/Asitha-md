5eaa7c2ce175b0339147f34fd296575f25ac56b5
hcsc4vz4y5-droid <hcsc4vz4y5@privaterelay.appleid.com>
2026-05-29 01:19:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,63 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
+const fs = require('fs-extra')
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER || '94789121558' ;
+global.mongodb = process.env.MONGODB_URI || "" ; 
+global.port=5000  ;
+global.audio = '' ; 
+global.video = '' ;
+global.blockJids = process.env.BLOCK_JID ||'120363169665426586@g.us' ;
+global.allowJids = process.env.ALLOW_JID ||'120363169665426586@g.us' ;
+global.email = 'astromedia0010@outlook.com' ; 
+global.github = 'https://github.com/Astropeda/Asta-Md' ;
+global.location = 'Sri Lanka' ;
+global.timezone  = process.env.TIME_ZONE || 'Asia/Colombo'
+global.gurl = '' ; 
+global.sudo =  process.env.SUDO || "94789121558" ;
+global.devs = "94789121558"; //Dont change it
+global.mztit = process.env.MZTIT ||"sasiya-md", 
+global.Gname = process.env.GNAME ||"sasiya-md-sᴜᴘᴘᴏʀᴛ",
+global.zyt = process.env.ZYT || '1',
+global.waUrl = process.env.WAURL ||"",
+global.website = '' ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' ;
 module.exports = {
-
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+  sessionName: process.env.SESSION_ID || '',
+  botname: process.env.BOT_NAME || 'sasiya-md',
+  ownername:  process.env.OWNER_NAME || `Sasiya`,
+  author:  process.env.PACK_AUTHER || 'Sasiya', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
+  packname:  process.env.PACK_NAME || "sasiya-md" , 
+  autoreaction: process.env.AUTO_REACTION || 'true',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'true', 
+  antifake :   process.env.FAKE_COUNTRY_CODE ||'212',
+  readmessage: process.env.READ_MESSAGE || 'true',
+  HANDLERS: process.env.PREFIX || '.',
+  warncount : process.env.WARN_COUNT || 2,
+  disablepm: process.env.DISABLE_PM || "false",
+  MsgsInLog:process.env.MSGS_IN_LOG ||'false',
+  pmMsgsInLog:process.env.PM_MSGS_IN_LOGS ||'false',
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true', 
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antilinkaction: process.env.BRANCH || 'remove',
+  BRANCH: process.env.BRANCH || 'Main',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "34wcCGPoe3yaGtpiBZgx4SN7",
+  caption :process.env.CAPTION || "\t*ᴘᴏᴡᴇʀᴇᴅ ʙʏ sasiya-md* ",
+  promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'true' ,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' ,
+  VERSION: process.env.VERSION || 'V.4.5.0',
+  LANG: process.env.THEME|| 'Asta_Md',
+  menu : process.env.MENU || '',
+  WORKTYPE: process.env.WORKTYPE || 'public'
 };
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
